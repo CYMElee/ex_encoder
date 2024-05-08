0ff90116e9c7055b3f8afe3fa96399aae093a245
Your Name <leesparks8@gmail.com>
2024-05-08 07:11:52 +0000

    finish

--- src/encoder/scripts/encoder.py
+++ src/encoder/scripts/encoder.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
-from std_msgs.msg import String 
+from std_msgs.msg import Float32MultiArray 
 import pigpio
 import time
 
@@ -12,8 +12,12 @@ RX_PIN = 14
 TX_PIN = 15
 BAUD_RATE = 115200
 
+str1 = 0.01
+str2 = 0.01
+str3 = 0.01
 
-msg = String()
+
+msg = Float32MultiArray()
 
 handle = pi.serial_open("/dev/ttyUSB0", BAUD_RATE)
 
@@ -25,7 +29,17 @@ def uart2rpi(data):
     data = data[start_index:] 
     end_index = data.find(";")
     data = data[:end_index+1]
-    return data
+    start = data.find('@')
+    ended = data.find(',')
+    str1 = float(data[start+1:ended-1])
+    start = ended
+    ended = data.find(',', start+1)
+    str2  = float(data[start+1:ended-1])
+    start = ended
+    ended = data.find(';', start+1)
+    str3  = float(data[start+1:ended-1])
+    msg.data = [str1, str2, str3]
+    return 0
 
 
 def clear_uart_buffer():
@@ -34,13 +48,13 @@ def clear_uart_buffer():
 
 def main():
     rospy.init_node('UART_FROM_STM32',anonymous=True)
-    pub = rospy.Publisher('encoder_str',String,queue_size=10)
+    pub = rospy.Publisher('encoder_value',Float32MultiArray,queue_size=10)
     clear_uart_buffer()
     time.sleep(5)
     rate = rospy.Rate(100)
     while not rospy.is_shutdown():
         (count, data) = pi.serial_read(handle, 100)
-        msg.data = uart2rpi(data)
+        uart2rpi(data)
         pub.publish(msg)
         rate.sleep()
 
